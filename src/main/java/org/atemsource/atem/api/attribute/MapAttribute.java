54a0c20b4fb8a8f286aedc653f99f58109450b5a
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-10-16 11:32:02 +0200

    added sorted to map attribute

--- src/main/java/org/atemsource/atem/api/attribute/MapAttribute.java
+++ src/main/java/org/atemsource/atem/api/attribute/MapAttribute.java
@@ -1,71 +1,49 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.api.attribute;
 
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-
 import org.atemsource.atem.api.type.Type;
+
 
 /**
  * The Interface MapAttribute offers manipulation methods for maps.
  * 
- * @param <K>
- *            the key type
- * @param <V>
- *            the target type
- * @param <R>
- *            the map type
+ * @param <K> the key type
+ * @param <V> the target type
+ * @param <R> the map type
  */
-public interface MapAttribute<K, V, R> extends Attribute<V, R> {
+public interface MapAttribute<K, V, R> extends Attribute<V, R>
+{
 
 	/**
 	 * Clear.
 	 * 
-	 * @param entity
-	 *            the entity
+	 * @param entity the entity
 	 */
 	public void clear(Object entity);
 
 	/**
 	 * Contains value.
 	 * 
-	 * @param entity
-	 *            the entity
-	 * @param element
-	 *            the element
+	 * @param entity the entity
+	 * @param element the element
 	 * @return true, if successful
 	 */
 	public boolean containsValue(Object entity, V element);
 
 	/**
-	 * Gets the collection sort type.
-	 * 
-	 * @return the collection sort type
-	 */
-	public CollectionSortType getCollectionSortType();
-
-	/**
 	 * Gets the element.
 	 * 
-	 * @param entity
-	 *            the entity
-	 * @param keye
-	 *            the keye
+	 * @param entity the entity
+	 * @param keye the keye
 	 * @return the element
 	 */
 	public V getElement(Object entity, K keye);
@@ -80,8 +58,7 @@ public interface MapAttribute<K, V, R> extends Attribute<V, R> {
 	/**
 	 * Gets the iterator.
 	 * 
-	 * @param entity
-	 *            the entity
+	 * @param entity the entity
 	 * @return the iterator
 	 */
 	public Iterator<Map.Entry<?, ?>> getIterator(Object entity);
@@ -89,8 +66,7 @@ public interface MapAttribute<K, V, R> extends Attribute<V, R> {
 	/**
 	 * Gets the key set.
 	 * 
-	 * @param a
-	 *            the a
+	 * @param a the a
 	 * @return the key set
 	 */
 	public Set<K> getKeySet(Object a);
@@ -105,8 +81,7 @@ public interface MapAttribute<K, V, R> extends Attribute<V, R> {
 	/**
 	 * Gets the key type.
 	 * 
-	 * @param key
-	 *            the key
+	 * @param key the key
 	 * @return the key type
 	 */
 	public abstract Type<K> getKeyType(K key);
@@ -114,41 +89,40 @@ public interface MapAttribute<K, V, R> extends Attribute<V, R> {
 	/**
 	 * Gets the size.
 	 * 
-	 * @param entity
-	 *            the entity
+	 * @param entity the entity
 	 * @return the size
 	 */
 	public int getSize(Object entity);
 
 	/**
+	 * Gets the collection sort type.
+	 * 
+	 * @return the collection sort type
+	 */
+	public boolean isSorted();
+
+	/**
 	 * Put element.
 	 * 
-	 * @param entity
-	 *            the entity
-	 * @param key
-	 *            the key
-	 * @param value
-	 *            the value
+	 * @param entity the entity
+	 * @param key the key
+	 * @param value the value
 	 */
 	public void putElement(Object entity, K key, V value);
 
 	/**
 	 * Removes the key.
 	 * 
-	 * @param entity
-	 *            the entity
-	 * @param key
-	 *            the key
+	 * @param entity the entity
+	 * @param key the key
 	 */
 	public void removeKey(Object entity, K key);
 
 	/**
 	 * Removes the value.
 	 * 
-	 * @param entity
-	 *            the entity
-	 * @param value
-	 *            the value
+	 * @param entity the entity
+	 * @param value the value
 	 */
 	public void removeValue(Object entity, V value);
 }
---
+++ src/main/java/org/atemsource/atem/spi/DynamicEntityTypeSubrepository.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ ******************************************************************************/
+package org.atemsource.atem.spi;
+
+import org.atemsource.atem.api.type.EntityTypeBuilder;
+
+
+/**
+ * DynamicEntityTypeSubrepository provide builders to create new EntityTypes.
+ * 
+ * @param <J> the generic type
+ */
+public interface DynamicEntityTypeSubrepository<J> extends EntityTypeSubrepository<J>
+{
+
+	/**
+	 * Creates the builder.
+	 * 
+	 * @param code the code
+	 * @return the entity type builder
+	 */
+	public EntityTypeBuilder createBuilder(String code);
+}

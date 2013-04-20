9dcbb5b4478e4c532eeaa7ffd13ca0b37fe3f7d3
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-04-20 06:55:40 +0200

    updated methd api

--- src/main/java/org/atemsource/atem/api/method/Method.java
+++ src/main/java/org/atemsource/atem/api/method/Method.java
@@ -2,13 +2,21 @@ package org.atemsource.atem.api.method;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.atemsource.atem.api.type.EntityType;
 import org.atemsource.atem.api.type.Type;
 
 public interface Method {
-public ParameterType getParameterType();
+	public ParameterType getParameterType();
 
-public Object invoke(Object target, Object[] param) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException;
+	public Object invoke(Object target, Object[] param) throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException;
 
-public Type<?> getReturnType();
+	public Type<?> getReturnType();
+
+	public EntityType<?> getEntityType();
+	
+	public java.lang.reflect.Method getJavaMethod();
+	
+	public String getCode();
 
 }
--- src/main/java/org/atemsource/atem/api/method/ParameterType.java
+++ src/main/java/org/atemsource/atem/api/method/ParameterType.java
@@ -3,7 +3,12 @@ package org.atemsource.atem.api.method;
 import org.atemsource.atem.api.attribute.Attribute;
 import org.atemsource.atem.api.type.EntityType;
 
-public interface ParameterType extends EntityType<Object[]>{
+public interface ParameterType extends EntityType<Object[]> {
 	int getParameterCount();
-	Attribute<?,?> getParameter(int index);
+
+	Attribute<?, ?> getParameter(int index);
+
+	public Method getMethod();
+
+	int indexOf(Attribute<?, ?> attribute);
 }

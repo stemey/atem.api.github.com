d761df7bf9a71812f5ea184c39d6063cea38bbd9
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2013-05-02 17:53:44 +0200

    improved services

--- src/main/java/org/atemsource/atem/api/service/IdentityService.java
+++ src/main/java/org/atemsource/atem/api/service/IdentityService.java
@@ -1,40 +1,34 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.api.service;
 
 import java.io.Serializable;
 
 import org.atemsource.atem.api.type.EntityType;
+import org.atemsource.atem.api.type.Type;
+
 
 // TODO: Auto-generated Javadoc
 /**
  * The Interface IdentityService.
  */
-public interface IdentityService {
+public interface IdentityService
+{
 
 	/**
 	 * Gets the id.
 	 * 
-	 * @param entityType
-	 *            the entity type
-	 * @param entity
-	 *            the entity
+	 * @param entityType the entity type
+	 * @param entity the entity
 	 * @return the id
 	 */
 	public <E> Serializable getId(EntityType<E> entityType, E entity);
 
+	public Type<?> getIdType(EntityType<?> entityType);
 
 }
--- src/main/java/org/atemsource/atem/api/service/PersistenceService.java
+++ src/main/java/org/atemsource/atem/api/service/PersistenceService.java
@@ -1,42 +1,36 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.api.service;
 
 import java.io.Serializable;
 
+import org.atemsource.atem.api.type.EntityType;
+
+
 // TODO: Auto-generated Javadoc
 /**
  * The Interface PersistenceService.
  */
-public interface PersistenceService {
+public interface PersistenceService
+{
 
 	/**
 	 * Insert.
 	 * 
-	 * @param entity
-	 *            the entity
+	 * @param entity the entity
 	 */
-	public Serializable insert(Object entity);
+	public <E> Serializable insert(EntityType<E> entityType, E entity);
 
 	/**
 	 * Checks if is persistent.
 	 * 
-	 * @param intermediate
-	 *            the intermediate
+	 * @param intermediate the intermediate
 	 * @return true, if is persistent
 	 */
-	public boolean isPersistent(Object entity);
+	public <E> boolean isPersistent(EntityType<E> entityType, E entity);
 }
